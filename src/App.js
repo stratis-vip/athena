26a95cc032a9011b2646f9f560efced8919dd63c
stratis-vip <stratis.vip@gmail.com>
2022-01-26 09:01:50 +0200

    auto patch message

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
     return (
         <div className="App">
            <Title />
-            <War />
+            {/*<War />*/}
             <Users/>
             <Teams/>
         </div>
